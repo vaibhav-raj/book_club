4843004c6bdd465b183b2c9a744fc1a38578aeb3
Mohd Tausif <mohammadtausif14@gmail.com>
2021-10-09 02:40:06 +0530

    login

--- src/Components/User/Login.jsx
+++ src/Components/User/Login.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import styles from "./login.module.css"
 
 const Login = () => {
     return (
-        <div>
-
+        <div className={styles.background}>
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }}
+                noValidate
+                autoComplete="off">
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </Box>
         </div>
     )
 }
